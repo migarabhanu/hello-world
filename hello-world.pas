1444e8dc0bd21957b4b41abf517d4bfeda718906
Migara Bhanu <76873393+migarabhanu@users.noreply.github.com>
2023-04-26 12:37:11 +0530

    Updated

--- hello-world.pas
+++ hello-world.pas
@@ -1,4 +1,19 @@
 program Hello;
 begin
+
+begin
   writeln('Hello, World!');
+  writeln('I am Migara Bhanu');
+end
+
+program name;
+var
+	response: char;
+begin
+	writeLn('What is your name');
+	
+	if response = 'y' then
+	begin
+		writeLn('Alright! Thank you !');
+	end;
 end.
